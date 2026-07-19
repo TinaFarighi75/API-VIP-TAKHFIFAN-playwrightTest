0cbf0140b489e56cb1b6fae441c625588d589499
Tina Farighi <tinafarighi75@gmail.com>
2026-07-19 12:21:02 +0330

    feat(vendor): add happy senario for mobile number who is my vendor now

--- tests/create vendor/Happy/searchMobileIsNotNew.spec.ts
+++ tests/create vendor/Happy/searchMobileIsNotNew.spec.ts
@@ -1,1 +1,48 @@
-import { test, expect } from "../../../utils/fixtures";
+import { test, expect } from "../../../utils/fixtures.js";
+import { users } from "../../../test-data/user-data.js";
+
+test("check mobile number is assign to vendor @smoke @vendor @vendor-merchant @regression", async ({
+  authApi,
+  vendorApi,
+  authTokenAdmin,
+}) => {
+  const userTest = users[4];
+
+  await vendorApi.searchBusinessSearchMobileNumberIsNewOrNotRequest(
+    authTokenAdmin,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    userTest.mobile,
+  );
+
+  const status = await authApi.getStatus();
+  expect(status).toBe(200);
+
+  const response = await vendorApi.getsearchBusinessSearchMobileNumberIsNewResponse();
+
+  expect(response.current_page).toBe(1);
+  expect(response.per_page).toBe(30);
+  expect(response.total_entries).toBe(1);
+  expect(response.data).toHaveLength(1);
+
+  expect(response.data[0]).toMatchObject({
+    id: String(userTest.businessId),
+    type: "business",
+    attributes: {
+      name: userTest.businessName,
+      v3_id: userTest.vendorId,
+    },
+  });
+});

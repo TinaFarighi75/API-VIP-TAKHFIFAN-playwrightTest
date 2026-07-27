deeed7c28784e54c81f270152d7e34fd5ebe0270
Tina Farighi <t.farighi@takhfifan.com>
2026-07-27 18:25:56 +0330

    fix(takhfifan_payment): add describe for happy senario takhfifan_payment task id VIP-4124

--- tests/takhfifan_payment/Happy/takhfifan_payment.spec.ts
+++ tests/takhfifan_payment/Happy/takhfifan_payment.spec.ts
@@ -1,74 +1,79 @@
 import { test, expect } from "../../../utils/fixtures.js";
 
-test("check takhfifan payment with amount 10T @takhfifan-payment ", async ({
-  takhfifanPaymentApi,
-  authTokenAdmin,
-}) => {
-  const apiResponse = await takhfifanPaymentApi.takhfifanPaymentRequest(
-    authTokenAdmin,100000,100776
+test.describe("check takhfifan payment  happy senario validation @takhfifan-payment  @regression", () => {
+  test("check takhfifan payment with amount 10T", async ({
+    takhfifanPaymentApi,
+    authTokenAdmin,
+  }) => {
+    const apiResponse = await takhfifanPaymentApi.takhfifanPaymentRequest(
+      authTokenAdmin,
+      100000,
+      100776,
+    );
 
-  );
+    expect(apiResponse.status()).toBe(200);
 
-  expect(apiResponse.status()).toBe(200);
+    const response = await takhfifanPaymentApi.getTakhfifanPaymentResponse();
 
-  const response = await takhfifanPaymentApi.getTakhfifanPaymentResponse();
-
-  expect(response).toHaveProperty("msg");
-  expect(response).toHaveProperty("id");
-  expect(typeof response.msg).toBe("string");
-  expect(typeof response.id).toBe("number");
-  expect(response.msg.length).toBeGreaterThan(0);
-  expect(response.id).toBeGreaterThan(0);
-  expect(response).toEqual({
-    msg: "created successfully",
-    id: expect.any(Number),
+    expect(response).toHaveProperty("msg");
+    expect(response).toHaveProperty("id");
+    expect(typeof response.msg).toBe("string");
+    expect(typeof response.id).toBe("number");
+    expect(response.msg.length).toBeGreaterThan(0);
+    expect(response.id).toBeGreaterThan(0);
+    expect(response).toEqual({
+      msg: "created successfully",
+      id: expect.any(Number),
+    });
   });
-});
-test("check takhfifan payment with amount 100MT @takhfifan-payment ", async ({
-  takhfifanPaymentApi,
-  authTokenAdmin,
-}) => {
-  const apiResponse = await takhfifanPaymentApi.takhfifanPaymentRequest(
-    authTokenAdmin,1000000000,100776
+  test("check takhfifan payment with amount 100MT", async ({
+    takhfifanPaymentApi,
+    authTokenAdmin,
+  }) => {
+    const apiResponse = await takhfifanPaymentApi.takhfifanPaymentRequest(
+      authTokenAdmin,
+      1000000000,
+      100776,
+    );
 
-  );
+    expect(apiResponse.status()).toBe(200);
 
-  expect(apiResponse.status()).toBe(200);
+    const response = await takhfifanPaymentApi.getTakhfifanPaymentResponse();
 
-  const response = await takhfifanPaymentApi.getTakhfifanPaymentResponse();
-
-  expect(response).toHaveProperty("msg");
-  expect(response).toHaveProperty("id");
-  expect(typeof response.msg).toBe("string");
-  expect(typeof response.id).toBe("number");
-  expect(response.msg.length).toBeGreaterThan(0);
-  expect(response.id).toBeGreaterThan(0);
-  expect(response).toEqual({
-    msg: "created successfully",
-    id: expect.any(Number),
+    expect(response).toHaveProperty("msg");
+    expect(response).toHaveProperty("id");
+    expect(typeof response.msg).toBe("string");
+    expect(typeof response.id).toBe("number");
+    expect(response.msg.length).toBeGreaterThan(0);
+    expect(response.id).toBeGreaterThan(0);
+    expect(response).toEqual({
+      msg: "created successfully",
+      id: expect.any(Number),
+    });
   });
-});
-test("check takhfifan payment with amount between 10T and 100MT @takhfifan-payment ", async ({
-  takhfifanPaymentApi,
-  authTokenAdmin,
-}) => {
-  const apiResponse = await takhfifanPaymentApi.takhfifanPaymentRequest(
-    authTokenAdmin,200000,100776
+  test("check takhfifan payment with amount between 10T and 100MT", async ({
+    takhfifanPaymentApi,
+    authTokenAdmin,
+  }) => {
+    const apiResponse = await takhfifanPaymentApi.takhfifanPaymentRequest(
+      authTokenAdmin,
+      200000,
+      100776,
+    );
 
-  );
+    expect(apiResponse.status()).toBe(200);
 
-  expect(apiResponse.status()).toBe(200);
+    const response = await takhfifanPaymentApi.getTakhfifanPaymentResponse();
 
-  const response = await takhfifanPaymentApi.getTakhfifanPaymentResponse();
-
-  expect(response).toHaveProperty("msg");
-  expect(response).toHaveProperty("id");
-  expect(typeof response.msg).toBe("string");
-  expect(typeof response.id).toBe("number");
-  expect(response.msg.length).toBeGreaterThan(0);
-  expect(response.id).toBeGreaterThan(0);
-  expect(response).toEqual({
-    msg: "created successfully",
-    id: expect.any(Number),
+    expect(response).toHaveProperty("msg");
+    expect(response).toHaveProperty("id");
+    expect(typeof response.msg).toBe("string");
+    expect(typeof response.id).toBe("number");
+    expect(response.msg.length).toBeGreaterThan(0);
+    expect(response.id).toBeGreaterThan(0);
+    expect(response).toEqual({
+      msg: "created successfully",
+      id: expect.any(Number),
+    });
   });
 });
